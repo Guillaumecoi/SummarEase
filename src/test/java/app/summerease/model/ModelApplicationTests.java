f0c30e8f2b8c62bb4cf30883cb917f11c1ccd6a7
Guillaume <guillaume@pop-os.localdomain>
2023-11-24 03:44:01 +0100

    first commit

---
+++ src/test/java/app/summerease/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package app.summerease.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
